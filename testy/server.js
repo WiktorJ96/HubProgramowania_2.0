920c0d2328192dbb963d6f38070598f4c08f08c3
Wiktor.J <testyrecenzje@gmail.com>
2024-10-31 20:15:49 +0100

    up

--- testy/server.js
+++ testy/server.js
@@ -1,26 +1,30 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const fs = require("fs/promises");
 
-// Obsługa plików statycznych z bieżącego katalogu
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/home.html");
-});
+fs.readFile("file1.txt", "utf-8")
+    .then((data1) => {
+  console.log("Zawartość file1.txt:", data1); // Wyświetlamy zawartość file1.txt
 
+        return fs.readFile("file2.txt", "utf-8")
+            .then((data2) => {
+    console.log("Zawartość file2.txt:", data2); // Wyświetlamy zawartość file2.txt
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+    // Łączymy zawartość obu plików
+    const mergedData = data1 + "\n" + data2;
+    console.log(mergedData); //
+    // Zapisujemy połączoną zawartość do pliku result.txt
+                return fs.writeFile("result.txt", mergedData);
+                
+    });
+        
+})
+.then(() => {
+    // Po zapisaniu pliku wyświetlamy komunikat o sukcesie
+    console.log('Operacja zakończona pomyślnie, wynik zapisany w result.txt');
+  })
+  .catch((error) => {
+    // Obsługa błędów
+    console.error('Błąd podczas przetwarzania plików:', error);
+  });
 
-app.post("/submit-form", (req, res) => {
-  const userName = req.body.userName;
-  const email = req.body.email;
-  const message = req.body.message;
-
-  console.log(`Imię: ${userName}, Email: ${email}, Wiadomość: ${message}`);
-  res.send("Formularz został wysłany pomyślnie!");
-});
-
-app.listen(port, () => {
-  console.log(`Serwer działa na porcie ${port}`);
-});
